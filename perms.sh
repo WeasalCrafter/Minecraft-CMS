a217de71151332eeb423b6780284db65587b2e43
Logan Fick <loganfick07@gmail.com>
2024-09-07 16:00:01 -0400

    Update perms.sh

--- perms.sh
+++ perms.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Set permissions and ownership for /var/www/backups
-sudo chmod -R 775 /var/www/backups
-sudo chown www-data:www-data /var/www/backups
+sudo chmod -R 775 /var/www/backups/freetime
+sudo chown www-data:www-data /var/www/backups/freetime
+
+sudo chmod -R 775 /var/www/backups/competition
+sudo chown www-data:www-data /var/www/backups/competition
 
 # Set permissions and ownership for /var/www/paper
 sudo chmod -R 775 /var/www/paper
@@ -11,6 +14,10 @@ sudo chown www-data:www-data /var/www/paper
 # Set permissions and ownership for /var/www/html
 sudo chmod -R 775 /var/www/html
 sudo chown www-data:www-data /var/www/html
+
+# Ser permissions and ownership for /var/www/paper/command.txt
+sudo chown www-data:www-data /var/www/html/command.txt
+sudo chmod 644 /var/www/html/command.txt
 
 # Output success message
 echo "Permissions Set Successfully"
